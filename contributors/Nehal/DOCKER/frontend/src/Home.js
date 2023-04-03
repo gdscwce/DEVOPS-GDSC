461f92c0d1b1f243c93b79d67bf0a082a6ed6a4b
= <nehalughade1221@gmail.com>
2023-04-03 12:31:32 +0530

    Nehal

---
+++ contributors/Nehal/DOCKER/frontend/src/Home.js
@@ -0,0 +1,23 @@
+import React, { useEffect } from 'react'
+import axios from 'axios'
+const Home = () => {
+    const [msg,setMsg] = React.useState('Click the button in home')
+    const handleClick=async() => {
+          try {
+            const response = await axios.get('http://localhost:3001')
+            console.log(response)
+            setMsg(response.data.msg)
+          }
+          catch (error) {
+            console.log(error)
+          }
+    }
+  return (
+    <div style={{margin:"28vh 28vw",alignItems:"center",justifyContent:"center"}}>
+        <h1>{msg}</h1>
+        <button onClick={handleClick} style={{backgroundColor:"blue",color:"white",borderRadius:"10px",padding:"10px",fontSize:"20px"}}>Click Me</button>
+    </div>
+  )
+}
+
+export default Home
