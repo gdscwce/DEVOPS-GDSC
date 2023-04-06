2d8b21214683d40ecd57b35ae7b8b97d0d160adc
ThunderSmoker <pitlawararadhya@gmail.com>
2023-04-06 10:32:13 +0530

    new assignmrnt

--- assignments/DOCKER/backend/app.js
+++ assignments/DOCKER/backend/app.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const cors = require('cors');
-
+const multer = require('multer');
 const app = express();
 app.use(cors())
+app.use('/uploads', express.static('uploads'));
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
+const multerStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, './uploads/');
+    },
+    filename: (req, file, cb) => {
+      const ext=file.mimetype.split('/')[1];
+      const name=file.originalname.split('.')[0];
+      cb(null,`${name}-${Date.now()}.${ext}`);
+    }
+    
+})
+const upload = multer({ storage: multerStorage });
+const uploadsettings=upload.single('myFile');
 
 app.get('/', (req, res) => {
     res.json({'msg': 'Hi HOME'})
@@ -14,6 +28,14 @@ app.get('/about', (req, res) => {
   });
 app.get('/contact', (req, res) => {
     res.json({'msg': 'Hi CONTACT'})
+  });
+app.post('/upload', uploadsettings,(req, res) => {
+    const file=req.file;
+    console.log(file);
+   
+    res.json({'msg': 'File uploaded successfully'})
+
+
   });
 
   app.listen('3001', () => { 
