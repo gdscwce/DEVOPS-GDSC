487f5977e51753dbf9ba7c9854aa7609708e316d
= <nehalughade1221@gmail.com>
2023-04-13 01:12:08 +0530

    done with proxy and image upload

---
+++ contributors/Nehal/assignment2/backend/app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
+const app = express();
+app.use(cors())
+app.use('/uploads', express.static('uploads'));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+const multerStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, './uploads/');
+  },
+  filename: (req, file, cb) => {
+    const ext = file.mimetype.split('/')[1];
+    const name = file.originalname.split('.')[0];
+    cb(null, `${name}-${Date.now()}.${ext}`);
+  }
+
+})
+const upload = multer({ storage: multerStorage });
+const uploadsettings = upload.single('myFile');
+
+app.use('*', (req, res, next) => {
+  // requested url
+  console.log(req.url);
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.json({ 'msg': 'Hi HOME' })
+});
+app.get('/about', (req, res) => {
+  res.json({ 'msg': 'Hi ABOUT' })
+});
+app.get('/contact', (req, res) => {
+  res.json({ 'msg': 'Hi CONTACT' })
+});
+app.post('/upload', uploadsettings, (req, res) => {
+  console.log("hitting it")
+  const file = req.file;
+  console.log(file);
+
+  res.json({ 'msg': 'File uploaded successfully' })
+
+
+});
+
+app.listen('3001', () => {
+  console.log('Server is running on port 3001');
+})
